038ba8667eb4022287a2e60b2ee9a8fb5484d7bc
kiruti <ronald.tunui@gmail.com>
2022-09-09 18:51:50 -0700

    my first c taask

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,38 @@
+#tdlib.h>
+
+#include <time.h>
+
+/* more headers goes there */
+
+
+
+/* betty style doc for function main goes there */
+
+int main(void)
+  
+{
+  
+  int n;
+  
+
+  
+  srand(time(0));
+  
+  n = rand() - RAND_MAX / 2;
+  
+  /* your code goes there */
+  if (n > 0)
+    {
+      printf("%d is positive/n", n);
+    }
+  else if (n<0)
+    {
+      printf("%d is a negative number ", n);
+	     }
+  else
+    {
+      printf("%d is zerro" ,n);
+    }
+  return (0);
+  
+}
