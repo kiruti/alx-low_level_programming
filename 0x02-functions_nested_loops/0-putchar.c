bacfb5add1000bd1eeb67b8b67a6182be7d55c14
kiruti <ronald.tunui@gmail.com>
2022-09-15 06:57:17 +0000

    we are at it again

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,20 @@
-#include <unistd.h>
 #include "holberton.h"
-
 /**
- * main - Check description
- * Description: It prints the word Holberton, followed by a new line.
- * Return: 0.
+ * main - Prints Holberton
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char word[9] = "Holberton";
-	int i;
-
-	for (i = 0; i < 9; i++)
-	{
-		_putchar(word[i]);
-	}
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
 	_putchar('\n');
-
 	return (0);
 }
