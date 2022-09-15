58a0ed49e162b6d05cb2f7dbe7e0ec02a431f09d
kiruti <ronald.tunui@gmail.com>
2022-09-15 02:23:22 +0000

    correcting number two

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,21 @@
-	#include "holberton.h"
+#include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * print_alphabet_x10 - Check holberton
+ * Description: function uses _putchar function to print
+ * alphabet in lowercase 10 times
+ * Return: Nothing.
  */
-int main(void)
+
+void print_alphabet_x10(void)
 {
-	    print_alphabet_x10();
-	        return (0);
+	char i;
+	int j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+			_putchar(i);
+		_putchar('\n');
+	}
 }
