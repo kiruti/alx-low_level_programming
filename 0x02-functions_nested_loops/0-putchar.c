c8a7135fe8a9bbe8b7d28ec1de14b7a1fa4a4e20
kiruti <ronald.tunui@gmail.com>
2022-09-15 17:11:25 +0000

    Correction on the value chain

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,24 @@
-#include <unistd.h>
-#include "main.h"
+#include "holberton.h"
 
 /**
- * main - Check description
- * Description: It prints the word Holberton, followed by a new line.
- * Return: 0.
+ * main - entry point
+ *
+ * Description: Prints Holberton with _putchar
+ *
+ * Return: always 0
  */
+
 int main(void)
 {
-	char word[9] = "Holberton";
-	int i;
+	int i = 0;
+	char c;
+	char s[] = "Holberton\n";
 
-	for (i = 0; i < 9; i++)
+	while (i <= 9)
 	{
-		_putchar(word[i]);
-	}	
-	_putchar('\n');
-
+		c = s[i];
+		_putchar(c);
+		i++;
+	}
 	return (0);
 }
