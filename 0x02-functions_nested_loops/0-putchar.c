6c0b6c860f616676883f8579e38b2814cf1bf9e8
kiruti <ronald.tunui@gmail.com>
2022-09-16 01:35:25 +0000

    we try to run like the world

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,21 @@
 #include "main.h"
 /**
- * main - Entry point
+ * main - print putchar
  *
  * Return: Alway 0 (Sucess)
  */
 
 int main(void)
 {
-	char *h = "salkerjoh\n";
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-	while (*h)
-	{
-		_putchar(*h);
-		h++;
-	}
 	return (0);
 }
