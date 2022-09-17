055ac73159b2d0e8f1edb50f3222a23d9fb74b19
kiruti <ronald.tunui@gmail.com>
2022-09-17 01:57:57 +0000

    let us hope that this shall work

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -16,7 +16,7 @@ void print_to_98(int n);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
-void print_numbers(void);
+void print_number(void);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
