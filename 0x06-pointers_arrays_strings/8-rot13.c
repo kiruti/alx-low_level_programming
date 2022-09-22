cd205914bffeb2b2dbc6d142b37fece9ddc452bc
kiruti <ronald.tunui@gmail.com>
2022-09-22 10:14:15 +0000

    We all run in circles

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -1,27 +1,26 @@
 #include "main.h"
 
 /**
- * rot13 -  a function that encodes a string using rot13.
- * @s: An input string to encode using rot13
- * Return: An encode string
+ * rot13 - check the code for Holberton School students.
+ * @str: String to be converted
+ * Return: Always 0.
  */
-char *rot13(char *s)
+char *rot13(char *str)
 {
-	int i = 0;
+	char regALPHA[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char altALPHA[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i, j;
 
-	while (s[i] != '\0')
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		while ((s[i] >= 'a' && s[i] <= 'z') ||
-				(s[i] >= 'A' && s[i] <= 'Z'))
+		for (j = 0; regALPHA[j] != '\0'; j++)
 		{
-			if ((s[i] >= 'a' && s[i] <= 'm') ||
-					(s[i] >= 'A' && s[i] <= 'M'))
-				s[i] += 13;
-			else
-				s[i] -= 13;
-			i++;
+			if (str[i] == regALPHA[j])
+			{
+				str[i] = altALPHA[j];
+				break;
+			}
 		}
-		i++;
 	}
-	return (s);
+	return (str);
 }
