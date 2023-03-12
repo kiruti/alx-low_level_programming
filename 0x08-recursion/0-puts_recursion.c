50086602c48aafc8e9db3a4bc24c57d615fde4a1
kiruti <ronald.tunui@gmail.com>
2023-03-12 09:55:32 -0700

    we tried to add values to s

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
-/*
+/**
  * void _puts_recursio - function that prints a string followed by a new line
  * @s is the value of arguments taken from the main function
  *
- */
+ **/
 
 void _puts_recursion(char *s)
 {
