4db5136197a0f276d6d597703a49997cec55efb1
kiruti <ronald.tunui@gmail.com>
2023-07-11 21:07:33 +0000

    Redoing malloc task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -5,23 +5,25 @@
 
 
 /**
+ * _strdup - function return pointer of duplicated string
  *
- * _strdup - function return poointer of duplicated string
  *
- *
- * @str: the string that will be duplicated 
+ * @str: the string that will be duplicated
+ * Return: it will return the pointer of the duplicated string
  **/
 
 char *_strdup(char *str)
 {
-
 	char *ptr;
 	int len = strlen(str);
 
+	if (str == NULL)
+		return (0);
 
-	ptr = (char*)malloc(len *sizeof(char));
 
-		strcpy(ptr , str);
+	ptr = (char *)malloc(len * sizeof(char));
+
+		strcpy(ptr, str);
 
 		return (ptr);
 
