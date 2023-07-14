86b989c9deacb70a970a3320cd4faecfa2d015a3
kiruti <ronald.tunui@gmail.com>
2023-07-14 06:15:49 +0000

    we run as fast as we coulld

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -3,40 +3,37 @@
 #include <stdlib.h>
 
 
-/** 
- *
+/**
  *  *_calloc - function to allocates memory
- *  @nmemb: unsigned int type 
- *  @size: unsigned int type 
- *  Return: return pointer to array 
+ *  @nmemb: unsigned int type
+ *  @size: unsigned int type
+ *  Return: return pointer to array
  *
- * */
+ **/
 
- void *_calloc(unsigned int nmemb, unsigned int size)
+void *_calloc(unsigned int nmemb, unsigned int size)
 {
 
-	unsigned int count; 
-	char *ptr; 
+	unsigned int count;
+	char *ptr;
 
 	if (nmemb == 0 || size == 0)
 		return (0);
 
-	ptr = malloc(nmemb * size); 
+	ptr = malloc(nmemb * size);
 
 	if (ptr ==  NULL)
 	{
 		return (NULL);
 	}
 
-	count = 0; 
+	count = 0;
 
-	
-		
 	while (count < nmemb * size)
 	{
 		ptr[count] = 0;
-		count++; 
+		count++;
 	}
-	return (ptr); 
+	return (ptr);
 
 }
