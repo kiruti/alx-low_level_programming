26b9c110ce52bb8d28e7b607949611366d7d2ed8
kiruti <ronald.tunui@gmail.com>
2023-08-02 19:23:17 +0000

    we run to live and die to be

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ *  *
+ *   * free_dog - function to free space
+ *    *
+ *     * @d: pointer to dog typedef type
+ *      * return: Always
+ *       *
+ *        **/
+
+void free_dog(dog_t *d)
+{
+
+	if (d == NULL)
+                return;
+
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
