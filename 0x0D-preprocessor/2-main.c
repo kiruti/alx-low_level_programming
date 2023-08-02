e0dd9c3478e94cbf889580bafafe0f58cd952914
kiruti <ronald.tunui@gmail.com>
2023-08-02 16:42:13 +0000

    correction main

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,14 +1,13 @@
-nclude <stdio.h>
+#include <stdio.h>
 
 /**
- *  * main - check the code
- *   *
- *    * Return: Always 0.
- *     */
+ *main - check the code
+ *
+ *Return: Always 0.
+ **/
 
 int main(void)
 {
 		printf("%s\n", __FILE__);
-			return (0);
+	return (0);
 }
-
