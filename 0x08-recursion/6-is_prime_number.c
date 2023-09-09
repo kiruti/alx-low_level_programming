5dfdbf65606d12996142ca49b4f6e54d493a2957
kiruti <ronald.tunui@gmail.com>
2023-09-09 20:20:42 +0000

    6-is_prime correction on Betty style coding

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * prime_check - checks to see if number is prime
- * @f: factor check
- * @p: possible prime number
- * 
- * Return: 1 if prime, 0 if not
- **/
+* prime_check - checks to see if number is prime
+* @f: factor check
+* @p: possible prime number
+*
+* Return: 1 if prime, 0 if not
+**/
 int prime_check(int f, int p)
 {
 	if (p < 2 || p % f == 0)
@@ -18,11 +18,11 @@ int prime_check(int f, int p)
 }
 
 /**
- * is_prime_number - states if number is prime
- * @n: number to check
- *
- * Return: 1 if prime, 0 if not
- **/
+* is_prime_number - states if number is prime
+* @n: number to check
+*
+* Return: 1 if prime, 0 if not
+**/
 int is_prime_number(int n)
 {
 	if (n == 2)
