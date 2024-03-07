8eef06d34f38b51cc3873d0ca30c893c324635d9
kiruti <ronald.tunui@gmail.com>
2024-03-07 20:17:12 +0000

    we are blue yet green

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,17 +1,17 @@
 /*
- *  * File: 0-print_list.c
- *   * Auth: Brennan D Baraban
- *    */
+ * File: 0-print_list.c
+ * Auth: Brennan D Baraban
+ */
 
 #include "lists.h"
 #include <stdio.h>
 
 /**
- *  * print_list - Prints all the elements of a list_t list.
- *   * @h: The list_t list.
- *    *
- *     * Return: The number of nodes in h.
- *      */
+ * print_list - Prints all the elements of a list_t list.
+ * @h: The list_t list.
+ *
+ * Return: The number of nodes in h.
+ */
 
 size_t print_list(const list_t *h)
 {
