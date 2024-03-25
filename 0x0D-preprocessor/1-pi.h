7a57639f3eb01f1cf1c65c8b2047328dcdd271e7
kiruti <ronald.tunui@gmail.com>
2024-03-25 06:58:18 +0000

    we ate greens

--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -2,11 +2,11 @@
 #define PI_H
 
 /*
- *  * File: 1-pi.h
- *   * Auth: Brennan D Baraban
- *    * Desc: Header file that defines a macro named PI
- *     *       as an abbreviation for the token 3.14159265359.
- *      */
+ * File: 1-pi.h
+ * Auth: Brennan D Baraban
+ * Desc: Header file that defines a macro named PI
+ *       as an abbreviation for the token 3.14159265359.
+ */
 
 #define PI 3.14159265359
 
